3b7e304abe8a7c6e0ae99df895d39ce473e95594
Maciej Konieczny <hello@narf.pl>
2010-03-28 13:27:48 +0200

    moved task creation form to a separate view

--- todo/app/controllers/tasks_controller.rb
+++ todo/app/controllers/tasks_controller.rb
@@ -1,12 +1,15 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
-    @new_task = Task.new
   end
 
   def create
     Task.new(params[:task]).save
     redirect_to :action => :index
+  end
+
+  def new
+    @task = Task.new
   end
 
   def show
