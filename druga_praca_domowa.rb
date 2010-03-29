3846dd7da4c30c73d6addb466aa322aaaf7323b9
Maciej Konieczny <hello@narf.pl>
2010-03-30 00:53:16 +0200

    added forth solution

--- druga_praca_domowa.rb
+++ druga_praca_domowa.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+require 'net/smtp'
+
+
 class DrugaPracaDomowa
   def self.zadanie1(how_many)
     Array.new(how_many) {|i| i ** 2}
@@ -13,9 +16,17 @@ class DrugaPracaDomowa
   def self.zadanie3(path)
     File.open(path).read
   end
+
+  def self.zadanie4(args)
+    smtp = Net::SMTP.new('')
+    arity = smtp.method(:check_auth_args).arity
+    smtp.send(:check_auth_args, *args[0...arity])
+    # `send` podpatrzone u http://github.com/yanekk
+  end
 end
 
 
 p DrugaPracaDomowa.zadanie1(6)
 p DrugaPracaDomowa.zadanie2('grgdm#5#gr#8#l#srgqlhv#gr#f}zduwhm#srwhjl')
 p DrugaPracaDomowa.zadanie3('README.markdown')
+p DrugaPracaDomowa.zadanie4(['foo', 'bar', 'baz'])
