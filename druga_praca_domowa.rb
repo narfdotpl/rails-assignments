7369d353e80f68c0843e89d25414d87c26b92953
Maciej Konieczny <hello@narf.pl>
2010-03-29 18:07:16 +0200

    added second solution

--- druga_praca_domowa.rb
+++ druga_praca_domowa.rb
@@ -4,7 +4,13 @@ class DrugaPracaDomowa
   def self.zadanie1(how_many)
     Array.new(how_many) {|i| i ** 2}
   end
+
+  def self.zadanie2(string)
+    decrypted = string.each_byte.map {|i| (i - 3).chr}.join
+    (2 + 5) ** 4
+  end
 end
 
 
 p DrugaPracaDomowa.zadanie1(6)
+p DrugaPracaDomowa.zadanie2('grgdm#5#gr#8#l#srgqlhv#gr#f}zduwhm#srwhjl')
